aefd81f0aa0f56621dc19896a24a2ac5ce7429eb
yuvi1114-wq <yuvi181810@gmail.com>
2026-02-04 08:41:07 +0530

    Implement linear search with comparison count

---
+++ Day3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    int comparisons = 0;
+    int foundIndex = -1;
+
+   
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Read key to search
+    scanf("%d", &k);
+
+   
+    for (int i = 0; i < n; i++) {
+        comparisons++;
+        if (arr[i] == k) {
+            foundIndex = i;
+            break;
+        }
+    }
+
+    
+    if (foundIndex != -1) {
+        printf("Found at index %d\n", foundIndex);
+    } else {
+        printf("Not Found\n");
+    }
+
+    printf("Comparisons = %d\n", comparisons);
+
+    return 0;
+}
