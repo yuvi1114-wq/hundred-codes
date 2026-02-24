f357266f644130d8a6eecf2778df7ee7ff07f88e
yuvi1114-wq <yuvi181810@gmail.com>
2026-02-24 12:48:20 +0530

    Add files via upload

---
+++ day14.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    
+    printf("Enter size of matrix: ");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int isIdentity = 1;
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(i == j) {
+                if(matrix[i][j] != 1) {
+                    isIdentity = 0;
+                    break;
+                }
+            } else {
+                if(matrix[i][j] != 0) {
+                    isIdentity = 0;
+                    break;
+                }
+            }
+        }
+        if(isIdentity == 0)
+            break;
+    }
+
+    if(isIdentity)
+        printf("Identity Matrix\n");
+    else
+        printf("Not an Identity Matrix\n");
+
+    return 0;
+}
