a4a8c05c1630e2df9339705828099ab702832bae
yuvi1114-wq <yuvi181810@gmail.com>
2026-02-25 09:08:37 +0530

    Add files via upload

---
+++ day17.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("Max: %d\n", max);
+    printf("Min: %d\n", min);
+
+    return 0;
+}
