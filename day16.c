f70a795b796e6905aa2dab99d1301f3d847f6730
yuvi1114-wq <yuvi181810@gmail.com>
2026-02-25 09:06:20 +0530

    Add files via upload

---
+++ day16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    int visited[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        visited[i] = 0;   
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(visited[i] == 1)
+            continue;
+
+        int count = 1;
+
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+                visited[j] = 1;
+            }
+        }
+
+        printf("%d:%d ", arr[i], count);
+    }
+
+    return 0;
+}
