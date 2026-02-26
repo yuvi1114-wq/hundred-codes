09f770e5fbe1677e41689b64f28203387bf6762a
yuvi1114-wq <yuvi181810@gmail.com>
2026-02-26 12:22:35 +0530

    Add zero sum subarray counter in day20.c
    Implement a program to count subarrays with zero sum.

---
+++ day20.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int count = 0;
+
+    for(int i = 0; i < n; i++) {
+        int sum = 0;
+        for(int j = i; j < n; j++) {
+            sum += arr[j];
+            if(sum == 0)
+                count++;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
