892dc11bdf1ea3d9649b79e6dcd082f5a127c1a4
yuvi1114-wq <yuvi181810@gmail.com>
2026-02-27 19:21:01 +0530

    Implement linked list with create, delete, and print functions

---
+++ day24.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure definition
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Create linked list
+struct Node* createList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for (int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    return head;
+}
+
+// Delete first occurrence of key
+struct Node* deleteKey(struct Node* head, int key) {
+    struct Node *temp = head, *prev = NULL;
+
+    // If head itself holds the key
+    if (temp != NULL && temp->data == key) {
+        head = temp->next;
+        free(temp);
+        return head;
+    }
+
+    // Search for the key
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    // If key not found
+    if (temp == NULL)
+        return head;
+
+    // Unlink node
+    prev->next = temp->next;
+    free(temp);
+
+    return head;
+}
+
+// Print linked list
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, key;
+
+    scanf("%d", &n);
+    struct Node* head = createList(n);
+
+    scanf("%d", &key);
+
+    head = deleteKey(head, key);
+
+    printList(head);
+
+    return 0;
+}
