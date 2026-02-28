91b7246effdf9ddf8c68eedc1d7b96d1093e910a
yuvi1114-wq <yuvi181810@gmail.com>
2026-02-28 17:06:57 +0530

    Add linked list functions and main program
    Implement linked list creation and count occurrences of a key.

---
+++ day25.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create linked list
+struct Node* createList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for (int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    return head;
+}
+
+// Function to count occurrences
+int countOccurrences(struct Node* head, int key) {
+    int count = 0;
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        if (temp->data == key) {
+            count++;
+        }
+        temp = temp->next;
+    }
+
+    return count;
+}
+
+int main() {
+    int n, key;
+
+    scanf("%d", &n);
+
+    struct Node* head = createList(n);
+
+    scanf("%d", &key);
+
+    int result = countOccurrences(head, key);
+
+    printf("%d\n", result);
+
+    return 0;
+}
